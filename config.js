555b5f1ef5c6a51973a546449792ce3eee3f8dde
heroku-miraheze <61781343+heroku-miraheze@users.noreply.github.com>
2020-07-18 15:28:20 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    token: "YOUR_DISCORD_BOT_TOKEN",
-    channel: "XXXXXXXXXXXXXXXXXX",
+    token: "NzM0MDM4MzM3NDU0NzM1NDMz.XxL4gQ.5BDDpSb4o4MKHs4r8Tz8cfiDR_Q",
+    channel: "734034513478418542",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
-    channel_id: "YOUTUBE_CHANNEL_ID",
+    channel_id: "UCopDi1R-N2Kuv1s8jPQ9rUQ",
     watchInterval: 30000
 }
