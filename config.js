4afe78bb5600b5b83c09794f987b4bba8c14f36c
heroku-miraheze <61781343+heroku-miraheze@users.noreply.github.com>
2020-07-18 15:35:42 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    token: "NzM0MDM4MzM3NDU0NzM1NDMz.XxL5sA.Fj4FRdhh0xgiByzbqBOhbzQ0cMQ",
+    token: "NzM0MDM4MzM3NDU0NzM1NDMz.XxL6EQ.ryJz_E9WrraChizFfkb-C0huaWc",
     channel: "734034513478418542",
     messageTemplate: "Hello @everyone, **{author}** just now uploaded a video **{title}**!\n{url}",
     channel_id: "UCopDi1R-N2Kuv1s8jPQ9rUQ",
